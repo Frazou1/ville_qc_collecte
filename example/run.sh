c490a10215f9fd21f95834e5675bb336942b32d1
Frazou1 <89617565+Frazou1@users.noreply.github.com>
2025-02-04 15:17:08 -0500

    Add calendar functionallity

--- example/run.sh
+++ example/run.sh
@@ -9,11 +9,13 @@ MQTT_HOST="$(jq -r '.mqtt_host' $OPTIONS_FILE)"
 MQTT_PORT="$(jq -r '.mqtt_port' $OPTIONS_FILE)"
 MQTT_USER="$(jq -r '.mqtt_username' $OPTIONS_FILE)"
 MQTT_PASS="$(jq -r '.mqtt_password' $OPTIONS_FILE)"
+CALENDAR_FILE="$(jq -r '.calendar_file' $OPTIONS_FILE)"  # Nouvelle option
 
 echo "[INFO] Démarrage de l'add-on Ville Québec Collecte"
 echo "[INFO] Adresse = $ADDRESS"
 echo "[INFO] Intervalle = $UPDATE_INTERVAL"
 echo "[INFO] MQTT = $MQTT_HOST:$MQTT_PORT"
+echo "[INFO] Fichier calendrier ICS = $CALENDAR_FILE"
 
 while true; do
     echo "[INFO] Exécution du script Python..."
@@ -22,7 +24,8 @@ while true; do
       --mqtt_host "$MQTT_HOST" \
       --mqtt_port "$MQTT_PORT" \
       --mqtt_user "$MQTT_USER" \
-      --mqtt_pass "$MQTT_PASS"
+      --mqtt_pass "$MQTT_PASS" \
+      --calendar_file "$CALENDAR_FILE"  # Passage du chemin du fichier ICS
 
     echo "[INFO] Attente $UPDATE_INTERVAL secondes..."
     sleep "$UPDATE_INTERVAL"
